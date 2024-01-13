2c6c06ef20f963420d4f3edb84d69b011970471e
Denovo <vo0v0000@naver.com>
2024-01-13 23:59:16 +0900

    refactor(#6): 의약품 허가목록 common 배치 최적화

--- medicine_search_engine/src/batch/medicine/medicineBatch.controller.ts
+++ medicine_search_engine/src/batch/medicine/medicineBatch.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
+import { MedicineCommonBatchService } from './services/medicineCommonBatch.service';
 import { MedicineDetailBatchService } from './services/medicineDetailBatch.service';
 @Controller('/batch/medicine')
 export class MedicineBatchController {
   constructor(
     private readonly medicineDetailBatchService: MedicineDetailBatchService,
+    private readonly medicineCommonBatchService: MedicineCommonBatchService,
   ) {}
 
   @Get('/update')
@@ -23,14 +25,14 @@ export class MedicineBatchController {
 
     return 'updateMedicine';
   }
-  // @Get('/update/image')
-  // async updateMedicineImage() {
-  //   console.log('updateMedicineImage start');
-  //   this.medicineBatchService.batchCommon().subscribe({
-  //     complete: () => console.log('updateMedicineImage complete'),
-  //     error: (error) => console.log('subErro', error.message, error.stack),
-  //     // next: (value) => console.log('subNext', value),
-  //   });
-  //   return 'updateMedicineImage';
-  // }
+  @Get('/update/image')
+  async updateMedicineImage() {
+    console.log('updateMedicineImage start');
+    this.medicineCommonBatchService.batch().subscribe({
+      complete: () => console.log('updateMedicineImage complete'),
+      error: (error) => console.log('subErro', error.message, error.stack),
+      // next: (value) => console.log('subNext', value),
+    });
+    return 'updateMedicineImage';
+  }
 }
--- medicine_search_engine/src/batch/medicine/services/medicineCommonBatch.service.ts
+++ medicine_search_engine/src/batch/medicine/services/medicineCommonBatch.service.ts
@@ -7,7 +7,17 @@ import { COMMON_API_URL_BUILD } from '@src/constant';
 import { Medicine } from '@src/type/medicine';
 import { renameKeys } from '@src/utils/renameKeys';
 import { typedEntries } from '@src/utils/typedEntries';
-import { bufferCount, map, mergeMap, of, retry } from 'rxjs';
+import {
+  bufferCount,
+  catchError,
+  concatMap,
+  from,
+  map,
+  mergeMap,
+  of,
+  retry,
+  tap,
+} from 'rxjs';
 
 @Injectable()
 export class MedicineCommonBatchService {
@@ -20,20 +30,23 @@ export class MedicineCommonBatchService {
   // ---------------------------------
   // BATCH
   // ---------------------------------
-  async batch(sort: 'ASC' | 'DESC' = 'ASC') {
+  batch(sort: 'ASC' | 'DESC' = 'ASC') {
     return this.fetchOpenApiCommonList$(1, sort).pipe(
       map((common) => this.convertOpenApiCommonToMedicineCommon$(common)),
       bufferCount(100),
-      mergeMap((common) => this.bulkCheckExistMedicine$(common)),
+      concatMap((common) => this.bulkCheckExistMedicine$(common)),
       mergeMap((c) => c),
       map((c) => this.checkUpdated$(c)),
-      mergeMap(({ common, updated }) =>
-        updated ? of(common) : this.uploadAndSetUpdatedImage$(common),
+      mergeMap(
+        ({ common, updated }) =>
+          updated ? this.uploadAndSetUpdatedImage$(common) : of(common),
+        50,
       ),
       map((common) => this.pickMedicineCommonData(common)),
       bufferCount(100),
-      mergeMap((common) => this.bulkUpdateMeidicineCommon(common)),
-      retry({ count: 3, delay: 5000 }),
+      mergeMap((common) => this.bulkUpdateMedicineCommon$(common), 1),
+    //   map((_, i) => console.log('batch', i)),
+      tap(() => console.log('rss MB', process.memoryUsage().rss / 1024 / 1024)),
     );
   }
 
@@ -70,9 +83,10 @@ export class MedicineCommonBatchService {
   }
 
   fetchImageBuffer$(imageUrl: string) {
-    return this.httpService
-      .get(imageUrl, { responseType: 'arraybuffer' })
-      .pipe(map(({ data }) => data));
+    return this.httpService.get(imageUrl, { responseType: 'arraybuffer' }).pipe(
+      map(({ data }) => data),
+      retry({ count: 3, delay: 5000 }),
+    );
   }
 
   /// ---------------------------------
@@ -107,22 +121,38 @@ export class MedicineCommonBatchService {
       .flat();
   }
 
-  async bulkUpdateMeidicineCommon(
+  async bulkUpdateMedicineCommon$(
     updateInput: (Prisma.medicineUpdateInput & { id: string })[],
   ) {
-    const updates = updateInput.map((input) => {
-      const { id, product_type, company_serial_number, image_url } = input;
-      const data = {
-        ...(product_type ? { product_type } : {}),
-        ...(company_serial_number ? { company_serial_number } : {}),
-        ...(image_url ? { image_url } : {}),
-      };
-      return this.prisma.medicine.update({
-        where: { id },
-        data,
-      });
-    });
-    return this.prisma.$transaction(updates);
+    return from(updateInput).pipe(
+      mergeMap((input) => {
+        const { id, product_type, company_serial_number, image_url } = input;
+        if (!product_type && !company_serial_number && !image_url) {
+          return of(null); // 아무 것도 하지 않음
+        }
+        const data = {
+          ...(product_type ? { product_type } : {}),
+          ...(company_serial_number ? { company_serial_number } : {}),
+          ...(image_url ? { image_url } : {}),
+        };
+        return from(
+          this.prisma.medicine.update({
+            where: { id },
+            data,
+          }),
+        ).pipe(
+          catchError((error) => {
+            console.error('Error updating medicine', error);
+            return of(null); // 에러 처리
+          }),
+        );
+      }, 1), // 동시성 제한
+      retry({ count: 3, delay: 5000 }),
+      catchError((error) => {
+        console.error('Error in bulk update', error);
+        return of(null); // 전체 작업에 대한 에러 처리
+      }),
+    );
   }
 
   /// ---------------------------------
@@ -145,6 +175,7 @@ export class MedicineCommonBatchService {
 
     const checkUpdated =
       !beforeImage?.includes(imageName) || beforeImage.includes('nedrug');
+
     if (checkUpdated) {
       return { common, updated: true };
     }
@@ -162,17 +193,20 @@ export class MedicineCommonBatchService {
 
     if (!imageName) return of(medicine);
     const imageKey = `medicines/${imageName}.jpg`;
-
+    const new_image_url = `https://${process.env.AWS_S3_BUCKET}.s3.ap-northeast-2.amazonaws.com/${imageKey}`;
     return this.fetchImageBuffer$(image).pipe(
       map((arrayBuffer) => Buffer.from(arrayBuffer)),
-      mergeMap((buffer) =>
-        this.s3Service.upload({
-          bucket: process.env.AWS_S3_BUCKET!,
-          key: imageKey,
-          file: buffer,
-        }),
+      mergeMap(
+        (buffer) =>
+          this.s3Service.upload({
+            bucket: process.env.AWS_S3_BUCKET!,
+            key: imageKey,
+            file: buffer,
+          }),
+        20,
       ),
-      map((imageUrl) => ({ ...medicine, image_url: imageUrl })),
+      map(() => ({ ...medicine, image: new_image_url })),
+      retry({ count: 3, delay: 5000 }),
     );
   }
 
@@ -181,7 +215,6 @@ export class MedicineCommonBatchService {
   ): Prisma.medicineUpdateInput & { id: string } {
     const { serial_number, company_serial_number, product_type, image } =
       medicine;
-
     return {
       id: serial_number,
       company_serial_number,
