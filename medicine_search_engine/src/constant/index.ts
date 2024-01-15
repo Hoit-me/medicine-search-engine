32c95a5ddc000cd57cf2f7960472ca338e890c12
Denovo <vo0v0000@naver.com>
2024-01-15 18:32:28 +0900

    feat: DUR 병용금기 - open api set

--- medicine_search_engine/src/constant/index.ts
+++ medicine_search_engine/src/constant/index.ts
@@ -1,11 +1,29 @@
 export const DETAIL_API_URL = `https://apis.data.go.kr/1471000/DrugPrdtPrmsnInfoService04/getDrugPrdtPrmsnDtlInq03?type=json`;
 export const COMMON_API_URL = `https://apis.data.go.kr/1471000/DrugPrdtPrmsnInfoService04/getDrugPrdtPrmsnInq04?type=json`;
 
-export const DETAIL_API_URL_BUILD = (API_KEY: string, pageNo: number) =>
-  DETAIL_API_URL + `&serviceKey=${API_KEY}&pageNo=${pageNo}&numOfRows=100`;
+export const DETAIL_API_URL_BUILD = (
+  API_KEY: string,
+  pageNo: number,
+  rows = 100,
+) =>
+  DETAIL_API_URL + `&serviceKey=${API_KEY}&pageNo=${pageNo}&numOfRows=${rows}`;
 //   `https://apis.data.go.kr/1471000/DrugPrdtPrmsnInfoService04/getDrugPrdtPrmsnDtlInq03?serviceKey=${API_KEY}&pageNo=${pageNo}&numOfRows=100&type=json`;
 
-export const COMMON_API_URL_BUILD = (API_KEY: string, pageNo: number) =>
-  COMMON_API_URL + `&serviceKey=${API_KEY}&pageNo=${pageNo}&numOfRows=100`;
+export const COMMON_API_URL_BUILD = (
+  API_KEY: string,
+  pageNo: number,
+  rows = 100,
+) =>
+  COMMON_API_URL + `&serviceKey=${API_KEY}&pageNo=${pageNo}&numOfRows=${rows}`;
 
 export const MEDICINE_IMAGE_BUCKET_FOLDER = 'medicines';
+
+export const DUR_COMBINED_API_URL =
+  'https://apis.data.go.kr/1471000/DURIrdntInfoService03/getUsjntTabooInfoList02?typeName=병용금기&type=json';
+export const DUR_COMBINED_API_URL_BUILD = (
+  API_KEY: string,
+  pageNo: number,
+  rows = 100,
+) =>
+  DUR_COMBINED_API_URL +
+  `&serviceKey=${API_KEY}&pageNo=${pageNo}&numOfRows=${rows}`;
