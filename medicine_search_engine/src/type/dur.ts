4dfa029297a04d9eece2daa97f384687d4411509
Denovo <vo0v0000@naver.com>
2024-01-16 12:16:14 +0900

    feat: DUR 병용금기 배치

--- medicine_search_engine/src/type/dur.ts
+++ medicine_search_engine/src/type/dur.ts
@@ -17,13 +17,36 @@ export namespace Dur {
         contraindication_eng_name: string; // 병용금기DUR성분(영문)
         contraindication_kor_name: string; // 병용금기DUR성분
         contraindication_mix: string; // 병용금기복합제
-        contraindication_related_ingredients: string; // 병용금기관계성분
+        contraindication_related_ingredient: string; // 병용금기관계성분
         contraindication_pharmacological_class: string; // 병용금기약효분류
         notification_date: string; // 고시일자
         prohibited_content: string; // 금기내용
         remarks: string; // 비고
         deletion_status: string; // 상태
       }
+
+      /**
+       * ## EXAMPLE
+       * <TYPE_NAME>병용금기</TYPE_NAME>
+       * <MIX_TYPE>단일</MIX_TYPE>
+       * <INGR_CODE>D000762</INGR_CODE>
+       * <INGR_ENG_NAME>Itraconazole</INGR_ENG_NAME>
+       * <INGR_KOR_NAME>이트라코나졸</INGR_KOR_NAME>
+       * <MIX/>
+       * <ORI>[M083733]이트라코나졸제피과립/[M083734]이트라코나졸/[M092870]이트라코나졸고체분산체/[M201487]이트라코나졸고체분산/[M201624]제피이트라코나졸과립</ORI>
+       * <CLASS>[06290]기타의 화학요법제</CLASS>
+       * <MIXTURE_MIX_TYPE>단일</MIXTURE_MIX_TYPE>
+       * <MIXTURE_INGR_CODE>D000419</MIXTURE_INGR_CODE>
+       * <MIXTURE_INGR_ENG_NAME>Lovastatin</MIXTURE_INGR_ENG_NAME>
+       * <MIXTURE_INGR_KOR_NAME>로바스타틴</MIXTURE_INGR_KOR_NAME>
+       * <MIXTURE_MIX/>
+       * <MIXTURE_ORI>[M083392]로바스타틴</MIXTURE_ORI>
+       * <MIXTURE_CLASS>[02180]동맥경화용제</MIXTURE_CLASS>
+       * <NOTIFICATION_DATE>20090303</NOTIFICATION_DATE>
+       * <PROHBT_CONTENT>횡문근융해증 </PROHBT_CONTENT>
+       * <REMARK/>
+       * <DEL_YN>정상</DEL_YN>
+       */
       export interface OpenApiDto {
         TYPE_NAME: string; // DUR유형
         MIX_TYPE: string; // 단일/복합
@@ -65,7 +88,7 @@ export namespace Dur {
         MIXTURE_INGR_ENG_NAME: 'contraindication_eng_name',
         MIXTURE_INGR_KOR_NAME: 'contraindication_kor_name',
         MIXTURE_MIX: 'contraindication_mix',
-        MIXTURE_ORI: 'contraindication_related_ingredients',
+        MIXTURE_ORI: 'contraindication_related_ingredient',
         MIXTURE_CLASS: 'contraindication_pharmacological_class',
         NOTIFICATION_DATE: 'notification_date',
         PROHBT_CONTENT: 'prohibited_content',
